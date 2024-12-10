bd233119a321d4d4bcb4c1dab7f91cb6d05cc3b3
Anastasiia Taushkanova <168531065+anastasiia-test@users.noreply.github.com>
2024-12-10 23:28:42 +0700

    feat: add skills

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,15 +1,21 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const testingSkillsDropBtn = document.querySelector('.testing_skills');
+const dropdownContent = document.querySelector('.dropdown-content');
+const arrow = document.querySelector('.fa-chevron-right');
+const softSkillsDropBtn = document.querySelector('.soft_skills');
+const softSkillDropdownContent = document.querySelector('.dropdown-content-1');
+const softSkillArrow = document.querySelector('.soft_skills h3 i.fa-chevron-right');
 
 hamburger.addEventListener("click", () => {
     // find out if click was made on the opened hamburger menu ( opened hamburger has class `active`).
     let crossIcon = document.querySelectorAll(".hamburger.active");
-    
-    if (hamburger && navMenu && crossIcon.length <= 0 ) {
+
+    if (hamburger && navMenu && crossIcon.length <= 0) {
         // hamburger menu is not open - let's open it!
         hamburger.classList.add("active");
         navMenu.classList.add("active")
-    } else if (hamburger && navMenu && crossIcon.length > 0 ) {
+    } else if (hamburger && navMenu && crossIcon.length > 0) {
         // hamburger menu is open - let's close it!
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
@@ -23,3 +29,20 @@ document.querySelectorAll(".nav-link").forEach(link => link.addEventListener("cl
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+testingSkillsDropBtn.addEventListener('click', () => {
+    if (dropdownContent.classList.contains('dropdown-content')) {
+        dropdownContent.classList.toggle('dropdown-content-active')
+        arrow.classList.toggle('fa-rotate-90')
+    }
+})
+
+softSkillsDropBtn.addEventListener('click', () => {
+    if (softSkillDropdownContent.classList.contains('dropdown-content-1')) {
+        softSkillDropdownContent.classList.toggle('dropdown-content-1-active')
+        softSkillArrow.classList.toggle('fa-rotate-90')
+    }
+})
+
+
+
